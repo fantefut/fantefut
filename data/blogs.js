a6c702a2e72a016afec15fe9bfc5e36ec834ac48
fantefut <gkoyuncu012@gmail.com>
2026-09-25 22:50:35 +0300

    site guncellemesi

--- data/blogs.js
+++ data/blogs.js
@@ -12,7 +12,7 @@ export const blogsData = [
         description: "Milli maç arasında fantezi lig kadrolarını doğrudan etkileyecek en yeni sakatlık haberleri ve Süper Lig teknik direktör değişiklikleri.",
         content: [
           "Milli maç arasında TFF Fantezi Lig menajerlerini yakından ilgilendiren önemli bir sakatlık haberi geldi. Milli takım kadrosunda yer alan Orkun Kökçü, sağ ayak parmağındaki ağrılar nedeniyle kadrodan çıkarıldı. Liglerin yeniden başlamasıyla birlikte kadrosunda Orkun'u bulunduran ya da transfer etmeyi düşünen teknik direktörlerin, bu sakatlık durumunu takip ederek (eğer son milli maçlarda kadroya alınmazsa) alternatif orta saha oyuncularına yönelmesi ve bütçe planlamasını buna göre yapması gerekiyor.",
-          "Milli aranın bir diğer flaş gelişmesi ise Süper Lig ekiften Çorum FK cephesinde yaşandı. Beklenmedik şekilde Uğur Uçar ile yolların ayrılmasının ardından teknik direktörlük görevine resmi olarak Burak Yılmaz getirildi. Öncesinde de Abdullah Avcı görüşmeleri ve anlaşamama haberlerini okuduk. Yeni hocasıyla ilk maçına deplasmanda çıkacak olan Çorum FK'nın antrenmanları, muhtemel 11 haberleri ve eğer olursa hücum hattındaki rol değişimleri bu geçiş döneminde çok sıkı takip edilmelidir."
+          "Milli aranın bir diğer flaş gelişmesi ise Çorum FK cephesinde yaşandı. Beklenmedik şekilde Uğur Uçar ile yolların ayrılmasının ardından teknik direktörlük görevine resmi olarak Burak Yılmaz getirildi. Öncesinde de Abdullah Avcı görüşmeleri ve anlaşamama haberlerini okuduk. Yeni hocasıyla ilk maçına deplasmanda çıkacak olan Çorum FK'nın antrenmanları, muhtemel 11 haberleri ve eğer olursa hücum hattındaki rol değişimleri bu geçiş döneminde çok sıkı takip edilmelidir."
         ]
       }
     ]
